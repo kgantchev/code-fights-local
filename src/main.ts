dffa64b6859fceee23fde0262f9a1835e4b28ef7
Kiril Gantchev <kgantchev@gmail.com>
2017-05-15 22:12:55 -0500

    Remove html sample and add more test tasks.

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { downloadTask } from './task-scraper';
 
 browserOptions.show = true;
 
-const url = {
+const testUrls = {
     wordLadder: 'https://codefights.com/interview/EDaACHNYHyH6qQFAL',
     visiblePoints: 'https://codefights.com/interview/wjewD7BPuQDhfa5yx',
     sumOfTwo: 'https://codefights.com/interview/qAL6AiSejoJZRNyox',
     sumSubsets: 'https://codefights.com/interview/kEgA4DXcfXuriqGru',
+    treeLevelSum: 'https://codefights.com/interview/oqQqEcGhoB2wHswx6',
+    sudoku2: 'https://codefights.com/interview/2szSXxzqWuAJKProX',
 };
 
-downloadTask(url.sumSubsets);
+downloadTask(testUrls.sudoku2);
 // downloadTask(url.visiblePoints);
